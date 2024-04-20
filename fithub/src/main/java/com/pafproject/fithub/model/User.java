63f60b7bdd7c3a5d30a4a1cf7a79c29147c93d5d
it21913860 <it21913860@my.sliit.lk>
2024-04-20 12:30:57 +0530

    Error Fixing of User Entity , implemented bi-directional relationship with MealPlan

---
+++ fithub/src/main/java/com/pafproject/fithub/model/User.java
@@ -0,0 +1,31 @@
+package com.pafproject.fithub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String provider;
+
+    @OneToMany(mappedBy = "user")
+    private List<MealPlan> mealPlanUsers;
+
+
+}
