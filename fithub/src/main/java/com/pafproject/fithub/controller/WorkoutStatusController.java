713e64e1656d4cbc72cc97241135dc740958f607
it21305382 <it21305382@my.sliit.lk>
2024-04-20 15:16:56 +0530

    delete method completed

--- fithub/src/main/java/com/pafproject/fithub/controller/WorkoutStatusController.java
+++ fithub/src/main/java/com/pafproject/fithub/controller/WorkoutStatusController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,11 @@ public class WorkoutStatusController {
         return new ResponseEntity<>(savedWorkoutStatus,HttpStatus.CREATED);
     }
 
-
+    @DeleteMapping("workoutstatus/delete/{id}")
+    public boolean delete(@PathVariable String id){
+        Long statusId = Long.valueOf(id);
+        workoutStatusRepo.deleteById(statusId);
+        return true;
+    }
 }
 
