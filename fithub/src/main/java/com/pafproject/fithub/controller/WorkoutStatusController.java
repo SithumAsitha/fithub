845259fa7cae876bc10591b372f6526bb5dfe6d5
it21305382 <it21305382@my.sliit.lk>
2024-04-20 12:26:31 +0530

    add GetMapping find with id to WorkoutStatusController

--- fithub/src/main/java/com/pafproject/fithub/controller/WorkoutStatusController.java
+++ fithub/src/main/java/com/pafproject/fithub/controller/WorkoutStatusController.java
@@ -1,7 +1,10 @@
 package com.pafproject.fithub.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pafproject.fithub.repo.WorkoutStatusRepo;
@@ -18,7 +21,13 @@ public class WorkoutStatusController {
         return "Workout Status Module";
     }
 
-    
+    @GetMapping("/workoutstatus/{id}")
+    public Optional show(@PathVariable String id){
+        Long statusId = Long.valueOf(id);
+        System.out.println(statusId);
+        return workoutStatusRepo.findById(statusId);
+    }
+
 
 }
 
