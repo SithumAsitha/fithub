ec500778032bf9df72fb8efba71e5a16ab86b781
it21913860 <it21913860@my.sliit.lk>
2024-04-20 12:27:14 +0530

    Dependancy Injection For MealPlanAPI . To Handle Database Connectivity With MealPlanRepository implemented with JPA Repository.
    Implemented Save process with repo to persist data comes with /save endpoint body

--- fithub/src/main/java/com/pafproject/fithub/api/MealPlanAPI.java
+++ fithub/src/main/java/com/pafproject/fithub/api/MealPlanAPI.java
@@ -2,12 +2,15 @@ package com.pafproject.fithub.api;
 
 
 
+import com.google.gson.Gson;
 import com.pafproject.fithub.dto.MealPlanDTO;
+import com.pafproject.fithub.model.MealPlan;
+import com.pafproject.fithub.model.User;
+import com.pafproject.fithub.repo.MealPlanRepository;
 import com.pafproject.fithub.util.MealTime;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,9 +19,31 @@ import java.util.List;
 @RequestMapping("/meal")
 public class MealPlanAPI {
 
+    private final MealPlanRepository repo;
+    public MealPlanAPI(MealPlanRepository repo){
+        this.repo = repo;
+    }
+
     @PostMapping(value = "/save",consumes = MediaType.APPLICATION_JSON_VALUE)
     public String saveMeal(@RequestBody MealPlanDTO obj){
         System.out.println(obj);
+
+        MealPlan mealPlan = new MealPlan();
+        mealPlan.setMealNames(new Gson().toJson(obj.getMealName()));
+        mealPlan.setMealTime(obj.getMealTime());
+        mealPlan.setDate(obj.getDate());
+
+        User user = new User();
+        user.setId(obj.getUserId());
+
+        mealPlan.setUser(user);
+        try {
+            repo.save(mealPlan);
+        }catch (Exception e){
+            e.printStackTrace();
+            return "not saved";
+        }
+
         System.out.println("RUN");
         return "OK";
     }
@@ -45,17 +70,17 @@ public class MealPlanAPI {
         List<String> mealNames3 = Arrays.asList("Spaghetti Bolognese", "Caesar Salad");
 
         MealPlanDTO meal1 = new MealPlanDTO();
-        meal1.setDate(LocalDate.of(2024, 4, 17));
+        //meal1.setDate(LocalDate.of(2024, 4, 17));
         meal1.setMealName(mealNames1);
         meal1.setMealTime(MealTime.BREAKFAST);
 
         MealPlanDTO meal2 = new MealPlanDTO();
-        meal2.setDate(LocalDate.of(2024, 4, 17));
+        //meal2.setDate(LocalDate.of(2024, 4, 17));
         meal2.setMealName(mealNames2);
         meal2.setMealTime(MealTime.LUNCH);
 
         MealPlanDTO meal3 = new MealPlanDTO();
-        meal3.setDate(LocalDate.of(2024, 4, 17));
+        //meal3.setDate(LocalDate.of(2024, 4, 17));
         meal3.setMealName(mealNames3);
         meal3.setMealTime(MealTime.DINNER);
 
