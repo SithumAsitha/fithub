3691b9a256eaa2360fbab7c96941c1d956347dc9
it21305382 <it21305382@my.sliit.lk>
2024-04-20 12:28:27 +0530

    add PostMapping to WorkoutStatusControllersController

--- fithub/src/main/java/com/pafproject/fithub/controller/WorkoutStatusController.java
+++ fithub/src/main/java/com/pafproject/fithub/controller/WorkoutStatusController.java
@@ -3,10 +3,15 @@ package com.pafproject.fithub.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pafproject.fithub.model.WorkoutStatus;
 import com.pafproject.fithub.repo.WorkoutStatusRepo;
 
 
@@ -26,6 +31,12 @@ public class WorkoutStatusController {
         Long statusId = Long.valueOf(id);
         System.out.println(statusId);
         return workoutStatusRepo.findById(statusId);
+    }
+
+    @PostMapping("/workoutstatus")
+    public ResponseEntity<WorkoutStatus> createExercise(@RequestBody WorkoutStatus workoutStatus){
+        WorkoutStatus savedWorkoutStatus = workoutStatusRepo.save(workoutStatus);
+        return new ResponseEntity<>(savedWorkoutStatus,HttpStatus.CREATED);
     }
 
 
