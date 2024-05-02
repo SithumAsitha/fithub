ac69470ab0d9e125fb0ca2e755ca770a6042f231
it21913860 <it21913860@my.sliit.lk>
2024-05-02 21:19:45 +0530

    Implement update delete and get all methods helping Repositories

--- fithub/src/main/java/com/pafproject/fithub/api/MealPlanAPI.java
+++ fithub/src/main/java/com/pafproject/fithub/api/MealPlanAPI.java
@@ -3,7 +3,9 @@ package com.pafproject.fithub.api;
 
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.pafproject.fithub.dto.MealPlanDTO;
+import com.pafproject.fithub.dto.MealSharing;
 import com.pafproject.fithub.model.MealPlan;
 import com.pafproject.fithub.model.User;
 import com.pafproject.fithub.repo.MealPlanRepository;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/meal")
@@ -50,47 +53,51 @@ public class MealPlanAPI {
 
     @DeleteMapping(value = "/delete")
     public String deleteMeal(@RequestParam(required = false) Integer id){
-        System.out.println(id);
-        System.out.println("RUN");
-        return "OK";
+        try {
+            repo.deleteById(id.longValue());
+            return "OK";
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "fail";
+
     }
 
     @PutMapping(value = "/update",consumes = MediaType.APPLICATION_JSON_VALUE)
     public String updateMeal(@RequestBody MealPlanDTO dto){
-        System.out.println(dto);
-        System.out.println("RUN");
+        MealPlan mealPlan = new MealPlan();
+        mealPlan.setId(dto.getId());
+        mealPlan.setMealNames(new Gson().toJson(dto.getMealName()));
+        mealPlan.setMealTime(dto.getMealTime());
+        mealPlan.setDate(dto.getDate());
+
+
+
+        try {
+            repo.save(mealPlan);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
         return "OK";
     }
 
     @GetMapping(value = "/get")
-    public List<MealPlanDTO> getAll(MealPlanDTO dto){
-
-        List<String> mealNames1 = Arrays.asList("Oatmeal", "Fruit Salad");
-        List<String> mealNames2 = Arrays.asList("Grilled Chicken", "Quinoa Salad");
-        List<String> mealNames3 = Arrays.asList("Spaghetti Bolognese", "Caesar Salad");
-
-        MealPlanDTO meal1 = new MealPlanDTO();
-        //meal1.setDate(LocalDate.of(2024, 4, 17));
-        meal1.setMealName(mealNames1);
-        meal1.setMealTime(MealTime.BREAKFAST);
-
-        MealPlanDTO meal2 = new MealPlanDTO();
-        //meal2.setDate(LocalDate.of(2024, 4, 17));
-        meal2.setMealName(mealNames2);
-        meal2.setMealTime(MealTime.LUNCH);
-
-        MealPlanDTO meal3 = new MealPlanDTO();
-        //meal3.setDate(LocalDate.of(2024, 4, 17));
-        meal3.setMealName(mealNames3);
-        meal3.setMealTime(MealTime.DINNER);
-
-        // Create ArrayList and add MealPackageDTO objects
-        List<MealPlanDTO> mealPlan = new ArrayList<>();
-        mealPlan.add(meal1);
-        mealPlan.add(meal2);
-        mealPlan.add(meal3);
-        System.out.println("RUN");
-        return mealPlan;
+    public List<MealSharing> getAll(MealPlanDTO dto){
+        List<MealPlan> all = repo.findAll();
+        List<MealSharing> collect = all.stream().map(ob -> {
+            MealSharing mealSharing = new MealSharing();
+            mealSharing.setMealPlanId(ob.getId());
+            mealSharing.setUserName(ob.getUser().getUsername());
+            ArrayList<String> o = new Gson().fromJson(ob.getMealNames(), new TypeToken<ArrayList<String>>() {
+            }.getType());
+            mealSharing.setMealName(o);
+            mealSharing.setTime(ob.getMealTime());
+            return mealSharing;
+        }).collect(Collectors.toList());
+        return collect;
     }
 
 }
